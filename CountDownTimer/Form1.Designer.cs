71768baf78ea2f78b5317d741d659eb1fda9fb8a
Wriju Ghosh <wghosh@microsoft.com>
2020-03-27 21:41:53 +0530

    second

---
+++ CountDownTimer/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CountDownTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numDDL = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numDDL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(331, 18);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(112, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTimer.Location = new System.Drawing.Point(12, 55);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(446, 128);
+            this.lblTimer.TabIndex = 1;
+            this.lblTimer.Text = "-00:00:00";
+            // 
+            // numDDL
+            // 
+            this.numDDL.Location = new System.Drawing.Point(189, 16);
+            this.numDDL.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numDDL.Name = "numDDL";
+            this.numDDL.Size = new System.Drawing.Size(105, 31);
+            this.numDDL.TabIndex = 2;
+            this.numDDL.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Total Seconds";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 203);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.numDDL);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.numDDL)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numDDL;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CountDownTimer/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CountDownTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int iSeconds = 60; //default 60 seconds 
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            iSeconds = Convert.ToInt32(numDDL.Value);
+            timer1.Enabled = true;
+            timer1.Interval = 1000; //One second interval 
+            timer1.Tick += Timer1_Tick;
+            lblTimer.Text = ConvertSecondsToHHMM(iSeconds);
+            lblTimer.ForeColor = Color.Black;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            iSeconds = iSeconds - 1;
+            ShakeForm();
+            if(iSeconds < 0) //if it goes below zero then make the font RED
+                lblTimer.ForeColor = Color.Red;
+
+            lblTimer.Text = this.Text = ConvertSecondsToHHMM(iSeconds);
+        }
+
+        private void ShakeForm()
+        {
+            int iLoop = 1000;
+
+            if (iSeconds == 0) //Shake it 
+            {
+                do
+                {
+                    this.Left -= 4;
+                    this.Left += 4;
+                    iLoop--;
+                } while (iLoop > 1);
+                iLoop = 1000;
+            }
+        }
+
+        //Get a Second and convert it to hh:mm:ss format
+        public string ConvertSecondsToHHMM(int _iSconds)
+        {
+            TimeSpan ts = new TimeSpan(0, 0, 0, iSeconds);
+            return ts.ToString();
+        }
+    }
+}
